c11195ea470de4a11a4d0f3b2243837fb81879fd
Atsocs <atsocs.cc@gmail.com>
2021-04-04 11:13:42 -0300

    Map Selection

--- cobrITA.py
+++ cobrITA.py
@@ -5,13 +5,14 @@ import pygame
 from game_state_machine.GameControl import GameControl
 from game_states.Credits import Credits
 from game_states.Help import Help
+from game_states.MapSelection import MapSelection
 from game_states.Menu import Menu
 
 pygame.init()
 screen = pygame.display.set_mode(screen_size := (480, 480))
 fps = 60
 
-states = [Menu(), Credits('Menu'), Help('Menu')]
+states = [Menu(), Credits('Menu'), Help('Menu'), MapSelection()]
 start_state = 'Menu'
 
 game = GameControl(states, start_state, screen, fps)
---
+++ game_states/MapSelection.py
@@ -0,0 +1,93 @@
+import pygame
+
+from definitions import background_color
+from game_state_machine.GameState import GameState
+
+
+class MapSelection(GameState):
+    def __init__(self):
+        super().__init__()
+        self.button_size = 100, 50
+
+        # Cores utilizadas no MapSelection
+        self.button_color = pygame.Color("yellow")
+        self.color_when_clicked = pygame.Color("white")
+
+        # Mensagem e Posição do canto superior esquerdo de cada botao
+        self.buttons = [
+            {'name': 'hall', 'msg': '1', 'pos': (350, 200)},  # hall do A
+            {'name': 'feijao', 'msg': '2', 'pos': (200, 100)},  # feijao
+            {'name': 'quadra', 'msg': '3', 'pos': (200, 300)},  # quadra do C
+            {'name': 'apto', 'msg': '4', 'pos': (50, 200)},  # apto do C-
+            {'name': 'menu', 'msg': 'Menu', 'pos': (10, 10)},
+        ]
+
+        self.rects = [(pygame.Rect(b['pos'], self.button_size), b) for b in self.buttons]
+
+    def startup(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, surface):
+        surface.fill(background_color)
+        for b in self.buttons:
+            self.draw_button(surface, b)
+
+    def draw_button(self, surface, button_info):
+        name = button_info['name']
+        pos = button_info['pos']
+        msg = button_info['msg']
+        size = self.button_size
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed(num_buttons=3)
+
+        rect = pygame.Rect(pos, self.button_size)
+        inside = rect.collidepoint(mouse)
+        color = self.color_when_clicked if inside else self.button_color
+        pygame.draw.rect(surface, color, rect)
+
+        # colocando texto no botão
+        small_text = self.fonts['h2']  # fixme: h2 or m?
+        text_surf, text_rect = self.text_objects(msg, small_text)
+        text_rect.center = rect.center
+        surface.blit(text_surf, text_rect)
+
+    def on_key_up(self, e):
+        pass
+
+    def on_mouse_up(self, e):
+        if e.button == 1:
+            collided, next_state = self.get_collisions(e.pos)
+            if collided:
+                self.next_state = next_state
+                self.done = True
+
+    def get_collisions(self, pos):
+        collided, msg = False, ''
+        for r, button in self.rects:
+            if r.collidepoint(pos):
+                msg = button['msg']
+                if msg.isnumeric():
+                    next_state = self.get_gameplay_state(button)
+                else:
+                    next_state = msg
+                return True, next_state
+        return False, None
+
+    @staticmethod
+    def get_gameplay_state(button):
+        return 'Gameplay' + button['name'].title().replace(' ', '')
+
+    @staticmethod
+    def text_objects(text, font):
+        """
+        Cria o retângulo com escrita (o botão)
+        """
+        text_surface = font.render(text, True, pygame.Color("black"))
+        return text_surface, text_surface.get_rect()
