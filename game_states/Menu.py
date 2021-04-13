5d9f47d5a5b414ed9eeebbb38803b2516f00dc91
Atsocs <atsocs.cc@gmail.com>
2021-04-13 20:08:03 -0300

    add achiev to menu

--- game_states/Menu.py
+++ game_states/Menu.py
@@ -24,10 +24,12 @@ class Menu(GameState):
 
     def draw(self, surface):
         surface.fill(background_color)
+
         surface.blit(self.title, self.title_rect)
         surface.blit(self.mapselec, self.mapselec_rect)
         surface.blit(self.help, self.help_rect)
         surface.blit(self.credits, self.credits_rect)
+        surface.blit(self.achiev, self.achiev_rect)
 
     def on_key_up(self, e):
         if e.key in [K_DOWN, K_RIGHT]:
@@ -61,18 +63,22 @@ class Menu(GameState):
         self.mapselec = f2.render(self.menus[1], True, pygame.Color("red"))
         self.help = f2.render(self.menus[2], True, pygame.Color("yellow"))
         self.credits = f2.render(self.menus[3], True, credits_color)
+        self.achiev = f2.render(self.menus[4], True, achiev_color)
 
     def set_rect_centers(self):
         self.set_texts()
+
         self.title_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] - 30)
-        self.credits_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 20)
-        self.help_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 40)
         self.mapselec_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1])
+        self.help_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 20)
+        self.credits_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 40)
         self.achiev_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 60)
+
         self.title_rect = self.title.get_rect(center=self.title_center)
+        self.mapselec_rect = self.mapselec.get_rect(center=self.mapselec_center)
         self.help_rect = self.help.get_rect(center=self.help_center)
         self.credits_rect = self.credits.get_rect(center=self.credits_center)
-        self.mapselec_rect = self.mapselec.get_rect(center=self.mapselec_center)
+        self.achiev_rect = self.achiev.get_rect(center=self.achiev_center)
 
     def select(self, idx):
         self.menus[idx] = '< ' + self.menus[idx] + ' >'
