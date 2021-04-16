73e58e3cd96d7675e9748cc170920ae2ad09b36e
Atsocs <atsocs.cc@gmail.com>
2021-04-16 00:18:19 -0300

    adding unused resources (maybe for future use or remotion)

---
+++ resources/maps/tsx/unused/tileset_marina.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="tileset_marina" tilewidth="32" tileheight="32" tilecount="52" columns="4">
+ <image source="png/4 BigSet.png" width="128" height="432"/>
+</tileset>
