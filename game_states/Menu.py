4ab55a22f9abd2b02fb957debce4c957dbdf7cc9
Atsocs <atsocs.cc@gmail.com>
2021-05-02 10:43:18 -0300

    removendo Credits e Achiev do Menu principal

--- game_states/Menu.py
+++ game_states/Menu.py
@@ -1,14 +1,14 @@
 import pygame
 from pygame.locals import *
 
-from definitions import background_color, credits_color, achiev_color
+from definitions import background_color
 from game_state_machine.GameState import GameState
 
 
 class Menu(GameState):
     def __init__(self):
         super().__init__()
-        self.menus = ["Main Menu", "Map Selection", "Help", "Credits", "Achievements"]
+        self.menus = ["Main Menu", "Map Selection", "Help"]
 
     def startup(self):
         self.selected = 1
@@ -28,8 +28,6 @@ class Menu(GameState):
         surface.blit(self.title, self.title_rect)
         surface.blit(self.mapselec, self.mapselec_rect)
         surface.blit(self.help, self.help_rect)
-        surface.blit(self.credits, self.credits_rect)
-        surface.blit(self.achiev, self.achiev_rect)
 
     def on_key_up(self, e):
         if e.key in [K_DOWN, K_RIGHT]:
@@ -58,9 +56,7 @@ class Menu(GameState):
         f1, f2 = (self.fonts[x] for x in ('h1', 'h2'))
         self.title = f1.render(self.menus[0], True, pygame.Color("blue"))
         self.mapselec = f2.render(self.menus[1], True, pygame.Color("red"))
-        self.credits = f2.render(self.menus[2], True, credits_color)
-        self.help = f2.render(self.menus[3], True, pygame.Color("yellow"))
-        self.achiev = f2.render(self.menus[4], True, achiev_color)
+        self.help = f2.render(self.menus[2], True, pygame.Color("yellow"))
 
     # noinspection DuplicatedCode
     def set_rect_centers(self):
@@ -68,15 +64,11 @@ class Menu(GameState):
 
         self.title_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] - 30)
         self.mapselec_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1])
-        self.credits_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 20)
         self.help_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 40)
-        self.achiev_center = (self.get_screen_rect().center[0], self.get_screen_rect().center[1] + 60)
 
         self.title_rect = self.title.get_rect(center=self.title_center)
         self.mapselec_rect = self.mapselec.get_rect(center=self.mapselec_center)
-        self.credits_rect = self.credits.get_rect(center=self.credits_center)
         self.help_rect = self.help.get_rect(center=self.help_center)
-        self.achiev_rect = self.achiev.get_rect(center=self.achiev_center)
 
     def select(self, idx, inplace=True):
         selected = '< ' + self.menus[idx] + ' >'
